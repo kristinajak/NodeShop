0af3e6ec51df12b2e7e61f4a444426c78fb865f1
kristinajak <kristina.jakubone@gmail.com>
2023-10-12 10:44:27 +0300

    initial commit before production

---
+++ routes/auth.js
@@ -0,0 +1,60 @@
+const express = require("express");
+const { check, body } = require("express-validator");
+
+const authController = require("../controllers/auth");
+const User = require("../models/user");
+
+const router = express.Router();
+
+router.get("/login", authController.getLogin);
+
+router.get("/signup", authController.getSignup);
+
+router.post(
+  "/login",
+  [
+    check("email", "Please enter your email").isEmail().normalizeEmail(),
+    check("password", "Please enter your password")
+      .trim()
+      .isLength({ min: 5 })
+      .isAlphanumeric(),
+  ],
+  authController.postLogin
+);
+
+router.post(
+  "/signup",
+  [
+    check("email")
+      .isEmail()
+      .withMessage("Please enter a valid email")
+      .custom((value, { req }) => {
+        return User.findOne({ email: value }).then((userDoc) => {
+          if (userDoc) {
+            return Promise.reject("Email already exists");
+          }
+        });
+      })
+      .normalizeEmail(),
+    body(
+      "password",
+      "Password must be min 5 characters and numbers and text only"
+    )
+      .isLength({ min: 5 })
+      .isAlphanumeric()
+      .trim(),
+    body("confirmPassword")
+      .custom((value, { req }) => {
+        if (value !== req.body.password) {
+          throw new Error("Passwords have to match");
+        }
+        return true;
+      })
+      .trim(),
+  ],
+  authController.postSignup
+);
+
+router.post("/logout", authController.postLogout);
+
+module.exports = router;
