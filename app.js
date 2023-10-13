8bb0c41330777bf9fae2c9f3ebba677b39c87e3b
kristinajak <kristina.jakubone@gmail.com>
2023-10-13 10:40:00 +0300

    manual SSL protection added

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const fs = require("fs");
+const https = require("https");
 
 const express = require("express");
 const bodyParser = require("body-parser");
@@ -20,7 +21,11 @@ const MONGODB_URI = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO
 
 const app = express();
 const store = new MongoDBStore({ uri: MONGODB_URI, collection: "sessions" });
+
 const csrfProtection = csrf();
+
+const privateKey = fs.readFileSync("server.key");
+const certificate = fs.readFileSync("server.cert");
 
 const fileStorage = multer.diskStorage({
   destination: (req, file, cb) => {
@@ -117,6 +122,8 @@ app.use((error, req, res, next) => {
 mongoose
   .connect(MONGODB_URI)
   .then(() => {
+    // https
+    //   .createServer({ key: privateKey, cert: certificate }, app)
     app.listen(3000);
   })
   .catch((err) => {
